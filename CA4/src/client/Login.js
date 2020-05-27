15f085bb44cd3fd334adab0740bef36668d8b4fb
SharonNeo <56033581+SharonNeo@users.noreply.github.com>
2020-05-27 04:43:28 +0100

    Update Login.js

--- CA4/src/client/Login.js
+++ CA4/src/client/Login.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
-import axios from '../../../CA4/node_modules/axios/../CA4/node_modules/axios';
+import axios from 'axios';
 
 class Login extends Component {
     constructor(props) {
